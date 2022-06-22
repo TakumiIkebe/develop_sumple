a7663a7bc6cb39647ca7a2997b05327970cb782b
TakumiIkebe <107160885+TakumiIkebe@users.noreply.github.com>
2022-06-22 15:42:43 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
-<script type="text/javascript">
-<!--
-document.getElementById("text").innerHTML = "ようこそ私のページへ";
-//-->
-</script>
+(function($){
+  $(document).ready(function(){
+    $("#add").on("click",function(){
+      if(parseInt($("#display").text())<100){
+    $("#display").text(parseInt($("#display").text())+1);
+    }})
+   $("#sub").on("click",function(){
+     if(parseInt($("#display").text())>0){
+    $("#display").text(parseInt($("#display").text())-1);
+    }});
+    $("#mul").on("click",function(){
+      if(parseInt($("#display").text())<50){
+    $("#display").text(parseInt($("#display").text())*2);
+    }else if(parseInt($("#display").text())>=50){
+      $("#display").text(100);
+    }});
+
+
+  });
+
+
+
+
+
+
+})(jQuery);
